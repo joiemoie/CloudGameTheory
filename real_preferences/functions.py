b110f39b23b9704ad6909eefa36cb737e3842ec4
Charon <jingyi.wang@sv.cmu.edu>
2019-04-16 23:37:59 -0700

    real_preferences_v1

---
+++ real_preferences/functions.py
@@ -0,0 +1,173 @@
+import numpy as np
+
+update_rate = .01
+
+# get preferred provider# according to preference distribution
+def get_preference(n):
+  winner = np.random.uniform(0.025, 0.035)
+  losers = [np.random.uniform(-0.01, 0.01) for _ in range(3)]
+  if n < 0.5:
+    return [winner] + losers
+  elif n < 0.79:
+    return losers[:1] + [winner] + losers[1:]
+  elif n < 0.92:
+    return losers[:2] + [winner] + losers[2:]
+  else:
+    return losers + [winner]
+
+def computeIsValid(user_profits, selected_provider):
+  # gets the max of the profits
+  user_profits_summed = np.sum(user_profits, 1)
+  user_max_profit = np.max(user_profits_summed, 1)
+  user_argmax_profit = np.argmax(user_profits_summed, 1)
+
+  # gets all of the users profits for a single provider
+  user_profit_by_provider = user_profits_summed[:, selected_provider]
+
+  isValid = (selected_provider == user_argmax_profit) * 1.0
+  isValid *= (user_profit_by_provider > 0) * 1.0
+  return isValid
+
+def profit_helper(provider_prices, provider_costs, selected_provider, selected_resource, max_prices, quantities, user_preferences):
+  num_providers = provider_prices.shape[0]
+  num_users = max_prices.shape[0]
+
+  provider_price = provider_prices[selected_provider, selected_resource]
+  provider_cost = provider_costs[selected_provider, selected_resource]
+  # user profits across all the providers
+  user_profits = user_utilities(max_prices, quantities, provider_prices, user_preferences)
+
+  isValid = computeIsValid(user_profits, selected_provider)
+  
+  return [isValid, provider_price, provider_cost, user_profits]
+
+
+def lower_profit(provider_prices, provider_costs, selected_provider, selected_resource, max_prices, quantities, user_preferences):
+
+  isValid, provider_price, provider_cost, user_profits = profit_helper(provider_prices, provider_costs, selected_provider, selected_resource, max_prices, quantities, user_preferences)
+
+  num_count = np.sum(isValid)
+  if (num_count == len(isValid)):
+    return [0.0, 0.0]
+
+  profit = np.sum(isValid) * (provider_price - provider_cost)
+
+  while ((np.sum(isValid) == num_count) and profit >= 0):
+    provider_price -= update_rate
+    user_profits[:, selected_resource, selected_provider] += update_rate * quantities[:,selected_resource]
+      # gets the max of the profits
+    isValid = computeIsValid(user_profits, selected_provider)
+    profit = np.sum(isValid) * (provider_price - provider_cost)
+
+  if (profit < 0):
+    provider_price += update_rate
+    user_profits[:, selected_resource, selected_provider] += update_rate * quantities[:,selected_resource]
+      # gets the max of the profits
+    isValid = computeIsValid(user_profits, selected_provider)
+    profit = np.sum(isValid) * (provider_price - provider_cost)
+  return [profit, provider_price]
+
+def same_profit(provider_prices,provider_costs, selected_provider, selected_resource, max_prices, quantities, user_preferences):
+  isValid, provider_price,provider_cost, user_profits = profit_helper(provider_prices,provider_costs, selected_provider, selected_resource, max_prices, quantities, user_preferences)
+
+  num_count = np.sum(isValid)
+  if (num_count == 0):
+    return [0.0, 0.0]
+
+
+  profit = np.sum(isValid) * (provider_price - provider_cost)
+
+  while (np.sum(isValid) == num_count and profit > 0):
+    #print(isValid, provider_price)
+    provider_price += update_rate
+
+    user_profits[:, selected_resource, selected_provider] -= update_rate * quantities[:,selected_resource]
+    isValid = computeIsValid(user_profits, selected_provider)
+    profit = np.sum(isValid) * (provider_price - provider_cost)
+
+
+  provider_price -= update_rate
+  profit = np.sum(isValid) * (provider_price - provider_cost)
+
+
+  return [profit, provider_price]
+
+def higher_profit(provider_prices,provider_costs, selected_provider, selected_resource, max_prices, quantities, user_preferences):
+  isValid, provider_price, provider_cost, user_profits = profit_helper(provider_prices, provider_costs, selected_provider, selected_resource, max_prices, quantities, user_preferences)
+
+  num_count = np.sum(isValid)
+  if (num_count < 1):
+    return [0.0, 0.0]
+  old_prov_privce = provider_price
+
+  profit = np.sum(isValid) * (provider_price - provider_cost)
+
+  while (np.sum(isValid) >= num_count - 1 and profit > 0):
+    provider_price += update_rate
+
+    user_profits[:, selected_resource, selected_provider] -= update_rate * quantities[:,selected_resource]
+    isValid = computeIsValid(user_profits, selected_provider)
+    profit = np.sum(isValid) * (provider_price - provider_cost)
+
+
+  if (provider_price-update_rate != old_prov_privce):
+    provider_price-= update_rate
+
+  user_profits[:, selected_resource, selected_provider] += update_rate
+  isValid = computeIsValid(user_profits, selected_provider)
+  profit = np.sum(isValid) * (provider_price - provider_cost)
+
+
+  return [profit, provider_price]
+
+def updated_price(provider_prices,provider_costs, selected_provider, selected_resource, max_prices, quantities, user_preferences):
+
+  low_prof, low_price = lower_profit(provider_prices,provider_costs, selected_provider, selected_resource, max_prices, quantities, user_preferences)
+  same_prof, same_price = same_profit(provider_prices,provider_costs, selected_provider, selected_resource, max_prices, quantities, user_preferences)
+
+  high_prof, high_price = higher_profit(provider_prices,provider_costs, selected_provider, selected_resource, max_prices, quantities, user_preferences)
+  #print(low_prof, same_prof, high_prof)
+  #print(low_price, same_price, high_price)
+
+  if (low_prof > same_prof and low_prof > high_prof):
+    return low_price
+  elif (high_prof >= same_prof):
+    return high_price
+  else:
+    return same_price
+
+
+def user_utilities(max_prices, quantities, provider_prices, user_preferences):
+  num_providers = provider_prices.shape[0]
+  num_users = max_prices.shape[0]
+  num_resource_types = max_prices.shape[1]
+
+  user_profits = np.tile(np.expand_dims(max_prices,2), [1, 1, num_providers]) +\
+                 np.tile(np.expand_dims(user_preferences,1), [1, num_resource_types, 1])
+  user_profits = user_profits - np.tile(np.expand_dims(provider_prices.transpose(),0), [num_users, 1, 1])
+  user_profits *= np.tile(np.expand_dims(quantities, 2), [1, 1, num_providers])
+
+  return user_profits
+
+# numpy function
+def user_assignments(provider_prices, max_prices, quantities, user_preferences):
+
+  assignments = np.full(max_prices.shape, -1)
+
+  utilities = user_utilities(max_prices, quantities, provider_prices, user_preferences)
+  utilities = np.sum(utilities, 1)
+  for i in range(len(assignments)):
+    user_benefits = utilities[i]
+    if np.amax(user_benefits) >=0:
+      assignments[i] = np.random.choice(np.flatnonzero(user_benefits == user_benefits.max()))
+    else:
+      assignments[i] = -1
+  return assignments
+    
+
+# numpy function
+def provider_profits(provider_prices, quantities,provider_costs, assignments):
+  result = np.zeros(len(provider_prices))
+  for i in range(len(provider_prices)):
+    result[i] = np.sum((assignments == i) * quantities * (provider_prices[i] - provider_costs[i]))
+  return result
---
+++ real_preferences/simulation.py
@@ -0,0 +1,100 @@
+import numpy as np
+import functions
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+# Set these parameters
+num_providers = 4
+num_users = 1000
+num_resource_types = 1
+
+# Set the max prices for the users
+random_prices = np.random.random((num_users, num_resource_types)) * 0.040 + 0.100
+
+# Set the initial starting prices for the companies
+final_prices = np.random.random((num_providers, num_resource_types)) * 0.040 + 0.100
+final_costs = np.full((num_providers, num_resource_types), 0.1)
+
+# Set the user preferences
+random_preferences = np.array([functions.get_preference(np.random.random()) for _ in range(num_users)])
+
+# function which converts price to quantity based on the demand function
+
+# Sets the quantities demanded
+quantities = np.array([[np.random.randint(10, 13) for _ in range(num_resource_types)] for _ in range(num_users)])
+
+# stores the final prices for each iteration
+results = np.zeros((1000, num_resource_types))
+
+# information for setting the numpy plot
+colors = cm.rainbow(np.linspace(0, 1, num_providers))
+fig=plt.figure(figsize=(8, 4))
+columns = 2
+rows = 1
+
+# iterates through 1000 iterations.
+for step in range(1000):
+  permutation = np.random.permutation(num_providers)
+  for perm in range(num_providers):
+
+    # random user makes the first move
+    i = permutation[perm]
+
+    #iterates through each resource type
+    for k in range(num_resource_types):
+
+      new_price = functions.updated_price(final_prices, final_costs, i, k, random_prices, quantities, random_preferences)
+
+      final_prices[i, k] = new_price
+
+    # determines the final assignments
+    assignments = functions.user_assignments(final_prices, random_prices, quantities, random_preferences)
+
+    # prints to the graph every 10 iterations
+    if (step % 10 == 0):
+
+      # computes the provider profits
+      prov_profits = functions.provider_profits(final_prices, quantities, final_costs, assignments)
+
+      # prints some useful intormation
+      print(i, prov_profits, final_prices, np.mean(random_preferences, axis=0))
+
+      # stores the results
+      results[step] = final_prices[i]
+
+      # plots the data
+      fig.add_subplot(rows, columns, 1)
+      plt.scatter(np.full((num_resource_types), step), results[step], c=colors[i])
+      fig.add_subplot(rows, columns, 2)
+      plt.scatter(step, prov_profits[i], c=colors[i])
+
+      plt.draw()
+
+      plt.pause(0.01)
+plt.show()
+
+
+## Market share results (2017)
+fig = plt.figure(figsize=(12, 4))
+labels = ['AWS', 'Azure', 'Google', 'IBM']
+
+# predict market share using model
+final_profits = prov_profits
+fig.add_subplot(1, 3, 1)
+plt.title("Prediction on model")
+plt.pie(final_profits, labels=labels, autopct = '%3.2f%%', colors=colors)
+
+# predict market share using real prices
+real_prices = np.array([[0.133], [0.100], [0.136], [0.137]]) # 2017 w SSD
+assignments = functions.user_assignments(real_prices, random_prices, quantities, random_preferences)
+final_profits = functions.provider_profits(real_prices, quantities, final_costs, assignments)
+fig.add_subplot(1, 3, 2)
+plt.title("Prediction on real prices")
+plt.pie(final_profits, labels=labels, autopct = '%3.2f%%', colors=colors)
+
+# real market share
+real_market_share = [0.6, 0.2, 0.0706, 0.1294]
+fig.add_subplot(1, 3, 3)
+plt.title("Real market share in 2017")
+plt.pie(real_market_share, labels=labels, autopct = '%3.2f%%', colors=colors)
+plt.show()
